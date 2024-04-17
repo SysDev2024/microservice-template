65fbeb14b533c29de482f51a4ef1bd3f80a5367e
Magnus411 <magnusgjerstad00@gmail.com>
2024-04-17 11:19:51 +0200

    creating test enpoint to show how to extract users

--- src/main/java/no/ntnu/microService/controller/template/TemplateController.java
+++ src/main/java/no/ntnu/microService/controller/template/TemplateController.java
@@ -1,6 +1,8 @@
 package no.ntnu.microService.controller.template;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -23,6 +25,13 @@ public class TemplateController {
     @GetMapping("Hello")
     public String hello() {
         return "Hello";
+    }
+
+    @GetMapping("testGetUser")
+    public String testGetUser() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String username = authentication.getName();
+        return "TestGetUser: " + username;
     }
 
 }
