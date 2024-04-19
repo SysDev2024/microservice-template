2b2d56270622439a0b927cc0736fe2f790e58a7c
Torgeir Haukaas <haukaas.torgeir@gmail.com>
2024-04-19 14:01:19 +0200

    feat: add OpenApi config file

---
+++ src/main/java/no/ntnu/microService/OpenApiConfig.java
@@ -0,0 +1,56 @@
+package no.ntnu.microService;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ *
+ * Configuration class that sets up the OpenAPI documentation for the BudgetPlan
+ * API.
+ */
+@Configuration
+
+public class OpenApiConfig {
+  /**
+   *
+   * Sets up the custom OpenAPI object with title, description, and version
+   * information.
+   *
+   * Also configures the security scheme to use a Bearer token for authentication.
+   *
+   * @return the custom OpenAPI object
+   */
+  @Bean
+  public OpenAPI customOpenAPI() {
+    OpenAPI openAPI = new OpenAPI();
+
+    Info info =
+        new Info()
+            .title("GoalService API")
+            .description(
+                "This is the template microservice for the Sparesti webapp. Remember to add a Bearer token when using endpoints")
+            .version("1.0.0");
+    openAPI.setInfo(info);
+
+    Components components = new Components();
+    components.addSecuritySchemes("bearerAuth",
+        new SecurityScheme()
+            .type(SecurityScheme.Type.HTTP)
+            .scheme("bearer")
+            .bearerFormat("JWT")
+            .description("Please enter a valid Bearer token to authenticate."));
+    openAPI.setComponents(components);
+
+    SecurityRequirement securityRequirement = new SecurityRequirement();
+    securityRequirement.addList("bearerAuth");
+    openAPI.addSecurityItem(securityRequirement);
+
+    return openAPI;
+  }
+}
+
